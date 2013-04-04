73359dfb8765f4ff42d642cf68fe93e0f19b8ea3
Alan Pies <pies@pies-mini.(none)>
2013-04-03 23:56:10 -0700

    clean up of non generator files

--- app/assets/javascripts/controllers/main_controller.coffee
+++
@@ -1,4 +0,0 @@
-MainController = ($scope) ->
-  $scope.awesomeThings = ['HTML5 Boilerplate', 'AngularJS', 'Karma' ]
-
-angular.module('AngularVelocityApp').controller( 'MainCtrl', [ '$scope', MainController ])
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-AngularVelocity::Application.config.secret_token = '8cbb8bb773947e37e8f7d7ff7aa205f89559e3b2a6107a7e9678cf6d0681bb28896723cc0f971c97e9f747a0e3d6bcafaadb6007410821f99749a8a243d64e09'
